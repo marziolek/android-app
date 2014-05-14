1cd06337dcf180879b188d9b7e6a08f6574b1eb5
Marcin Ziółek <ziolkenz@interia.pl>
2014-05-14 13:09:24 +0200

    Downloading users posts/stream and display in scroll view

--- Mgr/src/com/project/mgr/fragments/tabs/Stream_tab1.java
+++ Mgr/src/com/project/mgr/fragments/tabs/Stream_tab1.java
@@ -4,7 +4,9 @@
 package com.project.mgr.fragments.tabs;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,6 +28,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.app.ProgressDialog;
+import android.graphics.Movie;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -35,7 +38,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.facebook.Request;
 import com.facebook.Response;
@@ -76,7 +78,51 @@ public class Stream_tab1 extends Fragment {
         	return rootView;
     }
 	
-	TextView txt1,txt2,txt3,txt4;
+	class downloadFiles extends AsyncTask<String, String, Void> {
+		
+		private ProgressDialog progressDialog = new ProgressDialog(getActivity());
+	    InputStream is = null ;
+	    String result = "";
+	    protected void onPreExecute() {
+	       progressDialog.setMessage("Downloading files...");
+	       progressDialog.show();
+	       /*progressDialog.setOnCancelListener(new OnCancelListener() {
+			 @Override
+			  public void onCancel(DialogInterface arg0) {
+			  task.this.cancel(true);
+			    }
+			 });*/
+	     }
+
+		@Override
+	    protected Void doInBackground(String... params) {
+			downloadPosts(params[0],params[2]);
+			downloadPosts(params[0],params[3]);
+			if (downloadPosts(params[0],params[2]) && downloadPosts(params[0],params[3])) {
+				   final PreviewGifPlayer postGif = new PreviewGifPlayer(getActivity());
+				   try {
+			        	InputStream is = new FileInputStream(Environment.getExternalStorageDirectory().getPath() + "/MgrApp/stream/"+params[2]);
+			            byte[] array = streamToBytes(is);
+			    	    Movie movie = Movie.decodeByteArray(array, 0, array.length);
+			    	    postGif.setMovie(movie);
+			    	    getActivity().runOnUiThread(new Runnable() {
+			    	        @Override
+			    	        public void run() {
+			    	        	LinearLayout posts = (LinearLayout) getActivity().findViewById(R.id.posts);
+			    	        	posts.addView(postGif);
+			    	        }
+			    	   });
+				   } catch(Exception e) {
+					   System.out.println(e);
+				   }
+			   }
+			return null;
+		}
+		
+		protected void onPostExecute(Void v) {
+			this.progressDialog.dismiss();
+		}
+	}
 	
 	class RetrivePosts extends AsyncTask<String, String, Void> {
 		private ProgressDialog progressDialog = new ProgressDialog(getActivity());
@@ -127,56 +173,54 @@ public class Stream_tab1 extends Fragment {
 	    }
 	    
 	    protected void onPostExecute(Void v) {
-
-	  // ambil data dari Json database
-	  try {
-	   JSONArray Jarray = new JSONArray(result);
-	   LinearLayout posts = (LinearLayout) getActivity().findViewById(R.id.posts);
-	   String[] fields = {"user_id","created_at","gif","audio"};
-	   for(int i=0;i<Jarray.length();i++) {
-		   JSONObject Jasonobject = null;
-		   Jasonobject = Jarray.getJSONObject(i);
-		   
-		   for(int j=0;j<fields.length;j++) {
-		   
-		   TextView post = new TextView(getActivity());
-		   post.setText(Jasonobject.getString(fields[j]));
-		   post.setId(i);
-		   posts.addView(post);
-		   
-		   /*txt1 = (TextView) getActivity().findViewById(R.id.txt1);
-		   txt2 = (TextView) getActivity().findViewById(R.id.txt2);
-		   txt3 = (TextView) getActivity().findViewById(R.id.txt3);
-		   txt4 = (TextView) getActivity().findViewById(R.id.txt4);
-	
-		   Jasonobject = Jarray.getJSONObject(i);
-	
-		   //get an output on the screen
-		   String user_id = Jasonobject.getString("user_id");
-		   String created_at = Jasonobject.getString("created_at");
-		   String gif_name = Jasonobject.getString("gif");
-		   String audio_name = Jasonobject.getString("audio");
-
-		   txt1.setText(user_id);
-		   txt2.setText(created_at);
-		   txt3.setText(gif_name);
-           txt4.setText(audio_name);*/
+		  // ambil data dari Json database
+		  try {
+		   JSONArray Jarray = new JSONArray(result);
+		   //LinearLayout posts = (LinearLayout) getActivity().findViewById(R.id.posts);
+		  // String[] fields = {"user_id","created_at","gif","audio"};
+		   for(int i=0;i<Jarray.length();i++) {
+			   JSONObject Jasonobject = null;
+			   Jasonobject = Jarray.getJSONObject(i);
+			   
+			   String user_id = Jasonobject.getString("user_id");
+			   String created_at = Jasonobject.getString("created_at");
+			   String gif = Jasonobject.getString("gif");
+			   String audio = Jasonobject.getString("audio");
+			   
+			   String[] fields = {user_id,created_at,gif,audio};
+			   
+			   new downloadFiles().execute(fields);
+			   
+			   /*for(int j=0;j<fields.length;j++) {
+			   
+				   TextView post = new TextView(getActivity());
+				   post.setText(Jasonobject.getString(fields[j]));
+				   post.setId(i);
+				   posts.addView(post);
+			   
+				   Jasonobject = Jarray.getJSONObject(i);
+				   //get an output on the screen
+				   String user_id = Jasonobject.getString("user_id");
+				   String created_at = Jasonobject.getString("created_at");
+				   String gif_name = Jasonobject.getString("gif");
+				   String audio_name = Jasonobject.getString("audio");
+			   }*/
 		   }
-	   }
-	   this.progressDialog.dismiss();
-
-	  } catch (Exception e) {
-	   // TODO: handle exception
-	   Log.e("log_tag", "Error parsing data "+e.toString());
-	  }
-	}
+		   this.progressDialog.dismiss();
+	
+		  } catch (Exception e) {
+		   // TODO: handle exception
+		   Log.e("log_tag", "Error parsing data "+e.toString());
+		  }
+		}
 	}
 	
-	public void downloadPosts() {
+	public boolean downloadPosts(String user_id, String fileName) {
 		try {
 	        //set the download URL, a url that points to a file on the internet
 	        //this is the file to be downloaded
-	        URL url = new URL("http://somewhere.com/some/webhosted/file");
+			String fileToDownload = "http://wierzba.wzks.uj.edu.pl/~09_ziolekm/MgrApp/uploadedFiles/"+user_id+"/"+fileName;
+	        URL url = new URL(fileToDownload);
 
 	        //create the new connection
 	        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -191,10 +235,14 @@ public class Stream_tab1 extends Fragment {
 	        //set the path where we want to save the file
 	        //in this case, going to save it on the root directory of the
 	        //sd card.
-	        File SDCardRoot = Environment.getExternalStorageDirectory();
+	        String postsDir = Environment.getExternalStorageDirectory()+"/MgrApp/stream/";
+	        File SDCardRoot = new File(postsDir);
+	        if (!SDCardRoot.exists()) {
+	        	SDCardRoot.mkdirs();
+	        }
 	        //create a new file, specifying the path, and the filename
 	        //which we want to save the file as.
-	        File file = new File(SDCardRoot,"somefile.ext");
+	        File file = new File(SDCardRoot,fileName);
 
 	        //this will be used to write the downloaded data into the file we created
 	        FileOutputStream fileOutput = new FileOutputStream(file);
@@ -223,12 +271,33 @@ public class Stream_tab1 extends Fragment {
 	        }
 	        //close the output stream when done
 	        fileOutput.close();
-
+	        return true;
 		//catch some possible errors...
 		} catch (MalformedURLException e) {
 		        e.printStackTrace();
 		} catch (IOException e) {
 		        e.printStackTrace();
 		}
+		return false;
 	}
+	
+	private static byte[] streamToBytes(InputStream is) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream(1024);
+        byte[] buffer = new byte[1024];
+        int len;
+        try {
+            while ((len = is.read(buffer)) >= 0) {
+                os.write(buffer, 0, len);
+            }
+        } catch (java.io.IOException e) {
+        }
+        return os.toByteArray();
+    }
+	
+	
+	public void onGifClick(View v) {
+		PreviewGifPlayer gif = (PreviewGifPlayer) v;
+		gif.setPaused(!gif.isPaused());
+	}
+	
 }
